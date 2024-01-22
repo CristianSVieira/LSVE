d6da5c8c548cbe23b4d4248e5d77961ce17f96d4
LusoVilantino <lusovilantino@gmail.com>
2024-01-22 15:52:14 +0000

    Organiza fuccções da leitura de ficheiros

--- fnt/general/desbraga.h
+++ fnt/general/desbraga.h
@@ -5,7 +5,7 @@
 
 //#if defined(DESBRAGA) && DESBRAGA > 0
 
-#define LINHA_SALTAR() printf("%c", linha_salta) \
+#define LINHA_SALTAR() printf("%c", salta_linha) \
 
 #define MENSAGEM_DE_DESBRAGA(formato, ...) \
 printf("DESBRAGA ― %s:%d:%s: " formato, \
@@ -18,6 +18,10 @@ printf("DESBRAGA ― %s:%d:%s: " formato, \
 __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
 LINHA_SALTAR(); \
 LINHA_SALTAR()
+
+Vago quebra_o_programa(Vago) {
+	abort();
+}
 
 //#else
 //#define DESBRAGA_MENSAGEM(formato, ...) /* Faz nada em modo público */
